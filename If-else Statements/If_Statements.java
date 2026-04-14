e58e1f570516b695f68947596863299cfb95755b
Udit Kumar <udit56579@gmail.com>
2026-04-14 12:09:05 +0530

    Basic of If else

--- If-else Statements/If_Statements.java
+++ If-else Statements/If_Statements.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class If_Statements {
     public static void main(String[] args) {
-        int age = 20;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
 
         if (age >= 18) {
             System.out.println("You are eligible to vote");
+        } else if (age < 0) {
+            System.out.println("Invalid age");
+        } else if (age < 18) {
+            System.out.println("You are a Minor");
+        } else if (age == 0) {
+            System.out.println("You are not born yet");
+        } else {
+            System.out.println("You are not eligible to vote");
         }
-
+        scanner.close();
     }
 }
