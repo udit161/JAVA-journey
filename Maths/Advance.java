f20a788d2a4ffe76bcd9af05fdb8d695462fa940
Udit Kumar <udit56579@gmail.com>
2026-04-16 19:09:27 +0530

    coOrdinateGeometry

--- Maths/Advance.java
+++ Maths/Advance.java
@@ -4,17 +4,34 @@ public class Advance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double a, b, c;
-        System.out.println("Enter the coffecient of A , B :");
-        a = scanner.nextDouble();
-        b = scanner.nextDouble();
+        // double a, b, c;
+        // System.out.println("Enter the coffecient of A , B :");
+        // a = scanner.nextDouble();
+        // b = scanner.nextDouble();
 
-        c = Math.sqrt(a * a + b * b);
+        // c = Math.sqrt(a * a + b * b);
 
-        // c= Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        // // c= Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
-        System.out.println("The Hypotenuse is :" + c);
+        // System.out.println("The Hypotenuse is :" + c);
 
+        double radius;
+        System.out.println("Enter the radius of the circle : : ");
+
+        radius = scanner.nextDouble();
+
+        double circumference, area, volume;
+        System.out.println("Enter the radius of the circle : : ");
+
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.println("Circumference of the circle : " + circumference);
+        System.out.println("Area of the circle : " + area);
+        System.out.println("Volume of the sphere : " + volume);
         scanner.close();
 
     }
