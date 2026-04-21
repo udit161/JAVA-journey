2193d3a205b9a0c611451c00ef057ca969d857fd
Udit Kumar <udit56579@gmail.com>
2026-04-21 09:48:21 +0530

    SubstringTakingUserInputs

--- SubStrings/Main.java
+++ SubStrings/Main.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String email = "Udit123@gmail.com";
-        String username = email.substring(0, email.indexOf("@"));
-        System.out.println(username);
-        String domain = email.substring(email.indexOf("@") + 1);
-        System.out.println(domain);
+        // String email = "Udit123@gmail.com";
+        // String username = email.substring(0, email.indexOf("@"));
+        // System.out.println(username);
+        // String domain = email.substring(email.indexOf("@") + 1);
+        // System.out.println(domain);
+        Scanner src = new Scanner(System.in);
+        String email;
+        String username;
+        String domain;
+
+        System.out.println("Enter your email address: ");
+        if (email.contains("@")) {
+            email = src.nextLine();
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println("Username: " + username);
+            System.out.println("Domain: " + domain);
+        } else {
+            System.out.println("Invalid email address");
+        }
+
+        src.close();
+
     }
 
 }
