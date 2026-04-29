88cde64c39743088f1a26aafee45ef6c411fe959
Udit Kumar <udit56579@gmail.com>
2026-04-30 02:16:12 +0530

    temorature xconertor

--- TempratureCovertor/Main.java
+++ TempratureCovertor/Main.java
@@ -1,5 +1,26 @@
 package TempratureCovertor;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("ENTER THE TEMPRATURE: ");
+        temp = sc.nextDouble();
+
+        System.out.println("Convert to Celcius or Fahrenhite: ");
+        unit = sc.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5/9 : (temp * 9/5 + 32);
+
+        System.out.println(newTemp + unit);
+
+        sc.close();
+
+    }
 }
