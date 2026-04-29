e0f9fb84b9f2ff5334828094fc5ba4594dfe240d
Udit Kumar <udit56579@gmail.com>
2026-04-30 01:51:48 +0530

    ternaryOperators

---
+++ Ternary Operators/ternaryOperators.java
@@ -0,0 +1,20 @@
+public class ternaryOperators {
+    public static void main(String[] args) {
+
+        // int score = 55;
+        // String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        // System.out.println(passOrFail);
+
+        // int number = 3;
+        // String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+
+        // System.out.println();
+
+        int hours = 9;
+
+        String timeofDay = (hours < 12) ? "A.M." : "P.M.";
+
+        System.out.println(timeofDay);
+
+    }
+}
