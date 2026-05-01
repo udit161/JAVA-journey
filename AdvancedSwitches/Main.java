e6e3b858feb763b7ba87dd74ffed5907ccab3709
Udit Kumar <udit56579@gmail.com>
2026-05-02 01:55:45 +0530

    switches

---
+++ AdvancedSwitches/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String day = "Wednesday";
+
+        switch (day) {
+            case "Monday":
+                System.out.println("It is a weekday");
+                break;
+            case "Tuesday":
+                System.out.println("It is a weekday");
+                break;
+            case "Wednesday":
+                System.out.println("It is the day you were born!!!");
+                break;
+            case "Thursday":
+                System.out.println("It is a weekday");
+                break;
+            case "Friday":
+                System.out.println("It is a weekday");
+                break;
+            case "Saturday":
+                System.out.println("It is a weekend");
+                break;
+            case "Sunday":
+                System.out.println("It is a weekend");
+                break;
+        }
+    }
+
+}
